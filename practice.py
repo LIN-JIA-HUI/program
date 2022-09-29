8e83e988ae44ed08fee285fcf8e0b19a4103b832
LIN-JIA-HUI <97956573+LIN-JIA-HUI@users.noreply.github.com>
2022-09-29 08:25:21 +0800

    鏡頭連接&二分值白頭髮

---
+++ practice.py
@@ -0,0 +1,75 @@
+import cv2
+
+# ori = cv2.imread("hair.jpg")
+# img = cv2.imread("hair.jpg",cv2.IMREAD_GRAYSCALE)
+
+#二分值
+# thresh= 52
+# maxval=255
+
+# 旋轉角度
+# height,width = img.shape[0:2]
+# angular=input("請輸入旋轉角度:")
+# print(angular)
+# M = cv2.getRotationMatrix2D((width/2,height/2),angular,1)
+# dsize=(width,height)
+# dst1=cv2.warpAffine(img,M,dsize)
+
+# 鏡頭
+vid = cv2.VideoCapture('http://192.168.43.150:4747/mjpegfeed') # /video
+# cv2.namedWindow(cv2.WINDOW_NORMAL)
+while (vid.isOpened()):
+	
+	# Capture the video frame
+	# by frame
+	ret, frame = vid.read()
+	cv2.imshow('output',frame)
+
+	# Display the resulting frame
+    
+	# cv2.imshow('output', frame)
+	# gray_frame = cv2.cvtColor(frame,cv2.COLOR_BAYER_BG2BGR)
+	# thresh= 52
+	# maxval=255
+	# ret,dst=cv2.threshold(gray_frame,thresh,maxval,cv2.THRESH_BINARY)
+	# cv2.imshow("binary",dst)
+
+	c=cv2.waitKey(1)
+	if c == 13:
+		cv2.imwrite('mypict.jpg',frame)
+		img = cv2.imread("mypict.jpg",cv2.IMREAD_GRAYSCALE)
+		# gray_frame = cv2.cvtColor(img,cv2.COLOR_BAYER_BG2BGR)
+		#自己設定閾值
+		thresh= 100
+		maxval=255
+		ret,dst=cv2.threshold(img,thresh,maxval,cv2.THRESH_BINARY)
+		cv2.imshow('original',frame)
+		cv2.imshow("binary",dst)
+		#自適應閾值
+		dst_mean = cv2.adaptiveThreshold(img,maxval,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,3,5)
+		dst_gauss=cv2.adaptiveThreshold(img,maxval,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,-1)
+		cv2.imshow('adaptiveThreshold',dst_mean)
+		cv2.imshow("ADAPTIVE_THRESH_GAUSSIAN_C",dst_gauss)
+	if c==27:
+		break
+	
+	# the 'q' button is set as the
+	# quitting button you may use any
+	# desired button of your choice
+	# if cv2.waitKey(0) & 0xFF == ord('q'):
+	# 	break
+vid.release()
+cv2.destroyAllWindows()
+
+# ret,dst=cv2.threshold(img,thresh,maxval,cv2.THRESH_BINARY)
+# dst_mean=cv2.adaptiveThreshold(img,maxval,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,5,-1)
+# dst_gauss=cv2.adaptiveThreshold(img,maxval,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,-1)
+
+# cv2.imshow("original",ori)
+# cv2.imshow("gray",img)
+# cv2.imshow("thresh_binary",dst)
+# cv2.imshow("ADAPTIVE_THRESH_MEAN_C",dst_mean)
+# cv2.imshow("ADAPTIVE_THRESH_GAUSSIAN_C",dst_gauss)
+# # cv2.imshow("counterclockwise",dst1)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
