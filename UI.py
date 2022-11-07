7c1865f8ac8467736a214c2dc08068d9747a2c04
Liao H <melody9500302@gmail.com>
2022-11-07 13:05:42 +0800

    1107

--- UI.py
+++ UI.py
@@ -1,28 +1,48 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '0906b.ui'
+# Form implementation generated from reading ui file 'CameraWin.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import qdarkstyle
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1002, 740)
+        MainWindow.resize(1451, 737)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(50, 10, 1800, 1000))
-        self.label.setObjectName("label")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(20, 50, 161, 511))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton_4 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.verticalLayout.addWidget(self.pushButton_4)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.pushButton_3 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.verticalLayout.addWidget(self.pushButton_3)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem1)
+        self.pushButton_2 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.verticalLayout.addWidget(self.pushButton_2)
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(900, 1100, 131, 61))
+        self.pushButton.setGeometry(QtCore.QRect(500, 620, 301, 71))
         self.pushButton.setObjectName("pushButton")
+        self.image = QtWidgets.QLabel(self.centralwidget)
+        self.image.setGeometry(QtCore.QRect(200, 10, 900, 600))
+        self.image.setObjectName("image")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1002, 19))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1451, 19))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -32,11 +52,20 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        #setup style sheet
+        MainWindow.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5()) #介面改為黑色
+
+        #設定透明度
+        #MainWindow.setWindowOpacity(0.9)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "TextLabel"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.pushButton_4.setText(_translate("MainWindow", "Settings"))
+        self.pushButton_3.setText(_translate("MainWindow", "Suggestions"))
+        self.pushButton_2.setText(_translate("MainWindow", "Gray Hair"))
+        self.pushButton.setText(_translate("MainWindow", "Start"))
+        self.image.setText(_translate("MainWindow", "image"))
 
 
 if __name__ == "__main__":
@@ -47,4 +76,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
--- controller.py
+++ controller.py
@@ -1,13 +1,13 @@
-from PyQt5 import QtWidgets, QtGui, QtCore
+import time, sys
+from PyQt5 import QtWidgets, QtGui, QtCore, QtMultimediaWidgets
+from PyQt5.QtCore import pyqtSlot, Qt
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtMultimedia import (QCameraInfo, QCamera, QCameraImageCapture, QImageEncoderSettings, QMultimedia, QCameraViewfinderSettings, QVideoFrame, QSound)
+from PyQt5.QtMultimediaWidgets import QCameraViewfinder
 import cv2
-
 from cv2 import VideoCapture
-
 from UI import Ui_MainWindow
-
-from PyQt5 import QtCore, QtGui, QtWidgets
 
 class MainWindow_controller(QtWidgets.QMainWindow):
     def __init__(self):
@@ -17,60 +17,43 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.show_viedo)
         self.ui.pushButton.clicked.connect(self.videoButton)
-        self.ui.label.setText('image')
-        self.ui.pushButton.setText('video')
+        #self.ui.photo_Button.clicked.connect(self.photoButton)
         self.cap_video=0
         self.flag = 0
         self.img = []
 
+        
+
+        # self.camera = None  # QCamera对象
+        # cameras = QCameraInfo.availableCameras()
+        # if len(cameras) > 0:
+        #     self.__iniCamera()  # 初始化鏡頭
+        #     self.__iniImageCapture()  # 初始化靜態畫圖
+        #     self.camera.start()
+
     def videoButton(self):
         if (self.flag == 0):
-            self.cap_video = cv2.VideoCapture('http://192.168.221.242:4747/mjpegfeed')
+            self.cap_video = cv2.VideoCapture('http://192.168.158.244:4747/mjpegfeed')
             self.timer.start(5);
             self.flag+=1
             self.ui.pushButton.setText("Close")
         else:
             self.timer.stop()
             self.cap_video.release()
-            self.ui.label.clear()
-            self.ui.pushButton.setText("Open")
+            self.ui.image.clear()
+            self.ui.pushButton.setText("Continue")
             self.flag=0
     def show_viedo(self):
         ret, self.img = self.cap_video.read()
         if ret:
             self.show_cv_img(self.img)
     def show_cv_img(self, img):
-        shrink = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        shrink = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #將BGR格式轉為RGB格式
         QtImg = QtGui.QImage(shrink.data,
-                             shrink.shape[1],
-                             shrink.shape[0],
-                             shrink.shape[1] * 3,
-                             QtGui.QImage.Format_RGB888)
+                              shrink.shape[1],
+                              shrink.shape[0],
+                              shrink.shape[1] * 3,
+                             QtGui.QImage.Format_RGB888) #The image is stored using a 24-bit RGB format (8-8-8).
         jpg_out = QtGui.QPixmap(QtImg).scaled(
-            self.ui.label.width(), self.ui.label.height())
-
-        self.ui.label.setPixmap(jpg_out)
-
-
-    # def setup_control(self):
-    #     # TODO
-    #     # self.ui.pushButton.setText('Print message!')
-    #     # self.clicked_counter = 0
-    #     self.ui.button_line.clicked.connect(self.buttonClicked_line)
-    #     self.ui.button_text.clicked.connect(self.buttonClicked_text)
-    #     self.ui.button_plain.clicked.connect(self.buttonClicked_plain)
-    # def buttonClicked_line(self):
-    #     msg = self.ui.box_line.text()
-    #     self.ui.label_line.setText(msg)
-    # def buttonClicked_text(self):
-    #     msg = self.ui.box_text.toPlainText()
-    #     self.ui.label_text.setText(msg)
-    # def buttonClicked_plain(self):
-    #     msg = self.ui.box_plain.toPlainText()
-    #     self.ui.label_plain.setText(msg)
-
-    # def buttonClicked(self):
-    #     msg = self.ui.f2.text()
-    #     self.ui.f1.setText(msg)
-        # self.clicked_counter += 1
-        # print(f"You clicked {self.clicked_counter} times.")
+             self.ui.image.width(), self.ui.image.height())
+        self.ui.image.setPixmap(jpg_out)
