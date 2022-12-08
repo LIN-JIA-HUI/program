af439e23b53ce04332dbabf749d7b7a680fa8aec
LIN-JIA-HUI <lindahui0204@gmail.com>
2022-12-08 09:27:40 +0800

    hair test

--- practice.py
+++ practice.py
@@ -2,80 +2,74 @@ from cgitb import reset
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
-def window (name):
-    cv2.namedWindow(name,cv2.WINDOW_NORMAL)
-# def thresholding(img):
-#     thresh=130
-#     maxval=255
-#     ret,thresh=cv2.threshold(img,thresh,maxval,cv2.THRESH_BINARY)
 # def text(font):
 #     cv2.putText(ori,'white hair', (10,30),font,0.8,(255,0,0),2)
 # cv2.resizeWindow("res1", 500, 500) 
 
-# ori = cv2.imread("7.jpg")
-# img_gray = cv2.cvtColor(ori, cv2.COLOR_BGR2GRAY)
 # font = cv2.FONT_HERSHEY_SIMPLEX
+if __name__ == '__main__':
 
-# res1 = cv2.add(ori,ori)
-# res2 = ori +ori+ori
+    parse = argparse.ArgumentParser()
+    parse.add_argument('-i', '--imgpath', default='picture/2.jpg', help='Input image')
+    parse.add_argument('-o', '--output', default='', help='Output image')
+    parse.add_argument('--model', default='models/79999_iter.pth', help='model path')
+    parse.add_argument('--color', default='230,50,20', type=str, help='set bgr colors to change')
+    parse.add_argument('--show', default=1, type=int, help='Show picture')
 
-# 二分值
-thresh= 120
-maxval=255
-
+    args = parse.parse_args()
 # 鏡頭
-vid = cv2.VideoCapture('http://192.168.43.56:4747/mjpegfeed') # /video
+vid = cv2.VideoCapture('http://192.168.1.117:4747/mjpegfeed') # /video
 # cv2.namedWindow(cv2.WINDOW_NORMAL)
 while (vid.isOpened()):
-	
-	# Capture the video frame
-	# by frame
-	ret, frame = vid.read()
-	cv2.imshow('output',frame)
-	# Display the resulting frame
-    
-	c=cv2.waitKey(1)
-	if c == 13: #enter button
-		cv2.imwrite('mypict.jpg',frame)
-		break
-	if c==27: # esc button
-		break
-	# the 'q' button is set as the
-	# quitting button you may use any
-	# desired button of your choice
-	# if cv2.waitKey(0) & 0xFF == ord('q'):
-	# 	break
+
+# Capture the video frame
+# by frame
+ret, frame = vid.read()
+cv2.imshow('output',frame)
+# Display the resulting frame
+
+c=cv2.waitKey(1)
+if c == 13: #enter button
+	cv2.imwrite('mypict.jpg',frame)
+	break
+if c==27: # esc button
+	break
+# the 'q' button is set as the
+# quitting button you may use any
+# desired button of your choice
+# if cv2.waitKey(0) & 0xFF == ord('q'):
+# 	break
 vid.release()
 cv2.destroyWindow('output')
-img = cv2.imread("mypict.jpg")
+image_path = args.imgpath
+img = cv2.imread(image_path)
 thresh= 150
 maxval=255
-window("gray_hair")
-window("gray_hair_region")
+cv2.namedWindow("gray_hair",cv2.WINDOW_NORMAL)
+cv2.namedWindow("gray_hair_region",cv2.WINDOW_NORMAL)
+
 # 取高斯平滑
 img = cv2.GaussianBlur(img, (3, 3), 0)
 kernel = np.ones((3,3), np.uint8)
 erosion = cv2.erode(img, kernel, iterations = 1)
 img_gray = cv2.cvtColor(erosion, cv2.COLOR_BGR2GRAY)
 ret,dst=cv2.threshold(img_gray,thresh,maxval,cv2.THRESH_BINARY)
-contours,hierarchy=cv2.findContours(dst,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-image_copy = img.copy()
-cv2.drawContours(image_copy, contours, -1, (0, 255, 0),2, cv2.LINE_AA)
-cv2.imshow("dst",dst)
-cv2.imshow("image_copy",image_copy)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# cv2.imwrite('dst.jpg',dst)
-# cv2.imwrite('image_copy.jpg',image_copy)
-
-## 遮罩
-# ret,mask1=cv2.threshold(img_gray,thresh,maxval,cv2.THRESH_BINARY)
-# maskcolor = cv2.cvtColor(mask1)
-# res1 = cv2.add(ori,mask1)
-# print(f"threshold={ret}")
-# ori = cv2.bitwise_and(ori,ori,mask = mask1)
-# img = cv2.bitwise_and(img,img,mask = mask1)
-# cv2.addWeighted(ori,)
+# contours,hierarchy=cv2.findContours(dst,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+# image_copy = img.copy()
+# cv2.drawContours(image_copy, contours, -1, (0, 255, 0),2, cv2.LINE_AA)
+# cv2.imshow("gray_hair",dst)
+# cv2.imshow("gray_hair_region",image_copy)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
+
+image=cv2.imread(image_path)
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+lower_gray=np.array([0, 0, 40])
+upper_gray=np.array([180, 18, 230])
+mask = cv2.inRange(hsv, lower_gray, upper_gray)
+res = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("gray_hair",dst)
+cv2.imshow("gray_hair_region",res)
+cv2.waitKey(0)
