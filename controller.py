ee46b75d729fb53758acf41cbd639569aa4f8919
Liao H <melody9500302@gmail.com>
2022-12-08 10:01:18 +0800

    1208

--- controller.py
+++ controller.py
@@ -2,7 +2,7 @@ import time, sys
 import os
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets, QtGui, QtCore, uic
-from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QPixmap, QFont
 from PyQt5.QtWidgets import QFileDialog, QToolTip, QColorDialog
 import cv2
@@ -25,16 +25,17 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.bStart.clicked.connect(self.showVideo) #鏡頭
         self.bGrayHair.clicked.connect(self.grayHairAna) #白髮分析
         self.bPhoto.clicked.connect(self.takePhoto) #拍照
-        self.comboBox.addItems(["長髮整理", "中長髮整理", "短髮整理", "男生髮型整理", "其他"])
         self.comboBox.currentIndexChanged.connect(self.selectChanged)
-        self.bColor.clicked.connect(self.chooseColor) #調整髮色、唇色
-
-        # self.ui.comboBox.setCurrentIndex.connect(self.selectionchange)
-        self.image.setToolTip('手機鏡頭畫面')
-        self.photo.setToolTip('照片顯示於此')
+        # self.menuactionChoose_Hairstyle.triggered.connect(self.menuselectChanged)
+        self.bColor.clicked.connect(self.chooseColor) #調整髮色
+        self.actionClose.triggered.connect(exit) #關閉程式
+        self.actionOpen.triggered.connect(self.fileOpen) #開啟資料夾選擇照片
+        self.actionSave.triggered.connect(self.fileSave) #儲存照片至資料夾
+        self.lImage.setToolTip('手機鏡頭畫面')
+        self.lPhoto.setToolTip('照片顯示於此')
         self.bPhoto.setToolTip('拍照並儲存照片')
         self.bGrayHair.setToolTip('分析白頭髮區域')
-        self.bColor.setToolTip('使用調色盤選擇髮色及唇色')
+        self.bColor.setToolTip('使用調色盤選擇髮色')
         self.cap_video=0
         self.flag = 0
         self.img = []
@@ -42,9 +43,47 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         #setup style sheet
         #self.ui.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5()) #介面改為黑色
 
+    #整理方法建議
     def selectChanged(self, index):
-        # sindex = self.comboBox.currentIndex()
+        sindex = self.comboBox.currentIndex()
+        self.browser = QWebEngineView()
+        if sindex == 1:
+            self.browser.setWindowTitle('長髮整理')
+            self.browser.load(QtCore.QUrl('https://www.youtube.com/results?search_query=%E9%95%B7%E9%AB%AE%E6%95%B4%E7%90%86'))
+            self.browser.show()
+        elif sindex == 2:
+            self.browser.setWindowTitle('中長髮整理')
+            self.browser.load(QtCore.QUrl('https://www.youtube.com/results?search_query=%E4%B8%AD%E9%95%B7%E9%AB%AE%E6%95%B4%E7%90%86'))
+            self.browser.show()
+        elif sindex == 3:
+            self.browser.setWindowTitle('短髮整理')
+            self.browser.load(QtCore.QUrl('https://www.youtube.com/results?search_query=%E7%9F%AD%E9%AB%AE%E6%95%B4%E7%90%86'))
+            self.browser.show()
+        elif sindex == 4:
+            self.browser.setWindowTitle('男生髮型整理')
+            self.browser.load(QtCore.QUrl('https://www.youtube.com/results?search_query=%E7%94%B7%E7%94%9F%E9%AB%AE%E5%9E%8B%E6%95%B4%E7%90%86'))
+            self.browser.show()
+        elif sindex == 5:
+            self.browser.setWindowTitle('其他')
+            self.browser.load(QtCore.QUrl('https://www.youtube.com/results?search_query=%E9%AB%AE%E5%9E%8B%E6%95%B4%E7%90%86'))
+            self.browser.show()
+        else:
+            pass
+
+    def menuselectChanged(self):
         pass
+
+    #開啟資料夾選擇照片
+    def fileOpen(self):
+        filename = QFileDialog.getOpenFileName(self, 'Open File', 'c:\\', "Image Files(*.jpg *.png)")
+        imagePath = filename[0]
+        print(imagePath)
+        pixmap = QPixmap(imagePath)
+        self.lPhoto.setPixmap(pixmap)
+
+    #儲存照片至資料夾    
+    def fileSave(self):
+        filename = QFileDialog.getSaveFileName(self, 'Save File', 'c:\\', "Image Files(*.jpg *.png)")
 
     #連接手機相機
     def showVideo(self):
@@ -54,16 +93,10 @@ class MainWindow_controller(QtWidgets.QMainWindow):
             self.timer.start(5);
             self.flag+=1
             self.bStart.setText("Close")
-        # elif (self.flag == 1):
-        #     ret, frame = self.cap_video.read()
-        #     cv2.imwrite('photo.jpg',frame)
-        #     #cv2.imshow('photo.jpg', frame)
-        #     self.ui.bStart.setText("Close")
-        #     self.flag+=1
         else:
             self.timer.stop()
             self.cap_video.release()
-            self.image.clear()
+            self.lImage.clear()
             self.bStart.setText("Continue")
             self.flag=0
 
@@ -83,22 +116,14 @@ class MainWindow_controller(QtWidgets.QMainWindow):
             height, width, channel = frame.shape
             bytesPerline = channel * width
             img = QImage(frame, width, height, bytesPerline, QImage.Format_RGB888)
-            self.photo.setPixmap(QPixmap.fromImage(img))   # 顯示圖片
-            #window("photo")
-            #cv2.imshow("photo",frame)
-            #self.ui.image.setPixmap(QPixmap.fromImage(img))
-        # else:
-        #     self.ui.bPhoto.setToolTip('拍照')
+            self.lPhoto.setPixmap(QPixmap.fromImage(img))   # 顯示圖片
     
     #白頭髮 #合
     def grayHairAna(self):
         thresh= 120
         maxval= 255
         if (self.flag == 1):
-            #ret, frame = self.cap_video.read()
-            #cv2.imshow('output',frame)
-            #cv2.imwrite('photo.jpg')
-            #cv2.destroyWindow('output')
+            #下面兩行暫時註解掉。
             sc = self.selectedColor
             os.system(f'python makeup.py -i photo.jpg --color {sc[2]},{sc[1]},{sc[0]}')
             # img = cv2.imread("photo.jpg")
@@ -113,16 +138,6 @@ class MainWindow_controller(QtWidgets.QMainWindow):
             # cv2.drawContours(image_copy, contours, -1, (0, 255, 0),2, cv2.LINE_AA)
             # cv2.imshow("gray_hair",dst)
             # cv2.imshow("gray_hair_region",image_copy)
-
-    #整理方法建議
-    @pyqtSlot()
-    def selectionchange(self):
-        Form = QtWidgets.QWidget()
-        Form.setWindowTitle('整理方法建議')
-        self.browser = QWebEngineView()
-        # self.ui.comboBox.
-        self.browser.load(QtCore.QUrl('https://www.youtube.com/results?search_query=%E9%95%B7%E9%AB%AE%E6%95%B4%E7%90%86'))
-        self.browser.show()
 
     def chooseColor(self):
         color = QColorDialog.getColor()
@@ -146,5 +161,5 @@ class MainWindow_controller(QtWidgets.QMainWindow):
                               shrink.shape[1] * 3,
                              QtGui.QImage.Format_RGB888) #The image is stored using a 24-bit RGB format (8-8-8).
         jpg_out = QtGui.QPixmap(QtImg).scaled(
-             self.image.width(), self.image.height())
-        self.image.setPixmap(jpg_out)
+             self.lImage.width(), self.lImage.height())
+        self.lImage.setPixmap(jpg_out)
